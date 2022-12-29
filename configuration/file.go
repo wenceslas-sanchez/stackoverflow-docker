696afef11e22ce3fe3dee62a30fa4a71f18915f4
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-12-29 09:20:20 +0100

    ✨ save files

--- configuration/file.go
+++ configuration/file.go
@@ -1,6 +1,21 @@
 package configuration
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 type File interface {
 	ToString() (string, error)
 	ToBytes() ([]byte, error)
+}
+
+func Save(toFile File, name string, perm os.FileMode) error {
+	fileB, _ := json.MarshalIndent(toFile, "", " ")
+
+	if err := os.WriteFile(name, fileB, perm); err != nil {
+		return fmt.Errorf("can't save file: %q", err)
+	}
+	return nil
 }
